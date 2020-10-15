4c6bb1ef59406b98cf8e64bc4bd88f1cf5b9bf5b
Harrison Katz <github@hkatz.dev>
2020-10-15 10:50:33 -0700

    FEAT: Add Linus Torvalds Quote
    Shortened version of Linus's rant on C++

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os, time
 
 black = ["What makes people feel power.", "How to impress your friends", "Something overrated."]
 
-white = ["BTW I use Arch", "Blue switches sound", "Brainfuck"]
+white = ["BTW I use Arch", "Blue switches sound", "Brainfuck", "C++ is a horrible language. Quite frankly, even if the choice of C were to do *nothing* but keep the C++ programmers out,that in itself would be a huge reason to use C."]
 
 os.system('clear')
 
